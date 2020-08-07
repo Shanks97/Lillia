d2dabf8c5bd3b99d8a2058c56b9c993dec0b195b
Shanks97 <juanp_cm97@hotmail.com>
2020-08-06 23:59:56 -0500

    Updated token

--- Lillia/Program.cs
+++ Lillia/Program.cs
@@ -6,7 +6,7 @@ namespace Lillia
 {
     class Program
     {
-        public static DiscordClient Client = new DiscordClient(".", "NzQwMjgzODgyNzIwNzIyOTQ0.XymxFw.7hf8cGsk5YXCeDQ0I4TRFltKSe4");
+        public static DiscordClient Client = new DiscordClient(".", "NzQwMjgzODgyNzIwNzIyOTQ0.XymxFw.B2gZ6D3Gk7DX6gd9dDcHd01lRqE");
         static async Task Main(string[] args)
         {
             await Client.Run()
